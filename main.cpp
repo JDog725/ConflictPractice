77e3cef637d9ebde6e79ec85a371791a3d1691cd
Joseph Moran <tjmoran@nic.edu>
2024-10-23 10:30:31 -0700

    average function added to main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 using namespace std;
-
+int average(int a, int b, int c);
 int main()
 {
+    int num1, num2, num3;
     cout << "Hello World!\n";
+    cout << "Enter first integer: ";
+    cin >> num1;
+    cout << "Enter second integer: ";
+    cin >> num2;
+    cout << "Enter third integer: ";
+    cin >> num3;
+    cout << "The average of the three numbers is " <<average(num1, num2, num3) <<endl;
     return 0;
+}
+int average(int a, int b, int c)
+{
+    int sum = a + b + c;
+    double average = static_cast<double>(sum)/3;
+    return average;
 }
